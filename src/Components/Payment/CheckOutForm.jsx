01ebe953b8ac497630c412e898585bb2583fb5a0
RafiulAlamRami <rafiul7368@gmail.com>
2024-06-12 19:34:51 +0600

    resolve cupon problem

--- src/Components/Payment/CheckOutForm.jsx
+++ src/Components/Payment/CheckOutForm.jsx
@@ -29,7 +29,7 @@ export const CheckOutForm = ({ price, id, refetch }) => {
     const [er, setEr] = useState()
     const totalPrice = pr
 
-    const cuponRef = useRe(null);
+    const cuponRef = useRef(null);
     let cuponCode = '12345'
     const hancu = (e) => {
         e.preventDefault();
